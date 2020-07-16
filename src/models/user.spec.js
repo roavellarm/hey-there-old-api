faa34698a699652224d710393f9aa68ac1d0a6a7
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-15 23:44:01 -0300

    Fix user test

--- src/models/user.spec.js
+++ src/models/user.spec.js
@@ -2,15 +2,10 @@ import { expect } from 'chai'
 import User from './User'
 
 describe('User', () => {
-  // it('should exist', () => expect(User).to.exist)
-  // it('should be a function', () => expect(User).to.be.a('function'))
-  it('should have a name', () => expect(User).to.have.property('name'))
-  it('should have a email', () => expect(User).to.have.property('email'))
-  it('should have a password', () => expect(User).to.have.property('password'))
+  const user = new User()
+  it('should exist', () => expect(User).to.exist)
+  it('should be a function', () => expect(User).to.be.a('function'))
+  it('should have a name', () => expect(user).to.have.property('name'))
+  it('should have a email', () => expect(user).to.have.property('email'))
+  it('should have a password', () => expect(user).to.have.property('password'))
 })
-
-// Existe
-// tem que ser uma função
-// tem que ter o atributo name
-// tem que ter o atributo email
-// tem que ter o atributo password
