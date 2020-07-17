dc036365aa0361a461a55f63bca28ad05d9babca
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-17 00:58:06 -0300

    Move valitadion tests to tests folder

--- src/validations/tests/AuthValidations.spec.js
+++ src/validations/tests/AuthValidations.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { registerValidation } from './AuthValidations'
+import { registerValidation } from '../AuthValidations'
 
 describe('AuthValidations', () => {
   describe('registerValidation', () => {
