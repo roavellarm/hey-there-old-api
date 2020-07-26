d656e1b1e6f0397c8b4c80c62faf577aab5bc78e
Rodrigo Avellar <roavellarm@gmail.com>
2020-07-25 21:49:13 -0300

    Add ChatController

---
+++ src/controllers/ChatController.js
@@ -0,0 +1,75 @@
+import Chat from '../models/Chat'
+import User from '../models/User'
+import {
+  createChatValidations,
+  sendMessageValidations,
+  getMessagesValidations,
+} from '../validations/ChatValidations'
+
+function excludeIdsFromData(chat) {
+  return {
+    chatId: chat.chatId,
+    users: chat.users,
+    messages: chat.messages.map((msg) => {
+      return { message: msg.message }
+    }),
+  }
+}
+
+async function createChat(req, res) {
+  try {
+    const errors = await createChatValidations(req.body)
+    if (errors.length) return res.status(400).send({ error: errors })
+
+    const data = {
+      chatId: req.body.chatId,
+      users: req.body.users,
+    }
+    const chat = await Chat.create(data)
+
+    const users = await User.find({ users: req.body.users })
+
+    users.map((user) => {
+      user.chatList.push(chat.chatId)
+      return user.save()
+    })
+
+    return res.status(201).send({ message: 'Chat create successfuly!' })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function sendMessage(req, res) {
+  try {
+    const chatId = req.params.id
+    const { errors, chat, content, author } = await sendMessageValidations(
+      chatId,
+      req.body
+    )
+    if (errors.length) return res.status(400).send({ error: errors })
+
+    chat.messages.push({ message: { content, author } })
+    chat.save()
+
+    return res.status(200).send({ data: excludeIdsFromData(chat) })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+async function getMessages(req, res) {
+  try {
+    const { errors, chat } = await getMessagesValidations(
+      req.params.id,
+      req.body.currentUser
+    )
+    if (errors.length) return res.status(400).send({ error: errors })
+
+    return res.status(200).send({ data: excludeIdsFromData(chat) })
+  } catch (error) {
+    return res.status(400).send({ error })
+  }
+}
+
+export default { createChat, sendMessage, getMessages }
