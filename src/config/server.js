68db4f7e9d5b53554fbe22d5ed7cf0236aaecad1
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-24 00:10:33 -0300

    Update server and index

--- src/config/server.js
+++ src/config/server.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-console */
 import express from 'express'
+import socket from 'socket.io'
+import http from 'http'
 import morgan from 'morgan'
 import cors from 'cors'
 import path from 'path'
 
-function startServer(PORT, routes) {
+function startServer(PORT, IO_PORT, routes) {
   if (!PORT) throw Error(`Port is required to start server`)
   if (!routes) throw Error(`Routes are required to start server`)
 
   const app = express()
+  const server = http.createServer(app)
 
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
@@ -19,8 +23,23 @@ function startServer(PORT, routes) {
   app.use(cors())
   app.use('/', routes)
 
-  // eslint-disable-next-line no-console
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  const io = socket.listen(server)
+
+  io.on('connection', (sock) => {
+    console.log('A user is connected :)')
+
+    sock.on('chat message', (msg) => {
+      console.log(msg)
+      io.emit('chat message', msg)
+    })
+  })
+
+  server.listen(
+    IO_PORT,
+    console.log(`Socket server is running on port ${IO_PORT}`)
+  )
+
+  app.listen(PORT, () => console.log(`Http server running on port ${PORT}`))
 }
 
 export default startServer
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import routesList from './router/index'
 
 dotenv.config()
 
-const { DATABASE, PORT } = process.env
+const { DATABASE, PORT, IO_PORT } = process.env
 
 startDatabase(DATABASE)
 
-startServer(PORT, routesList)
+startServer(PORT, IO_PORT, routesList)
 
 export default { startServer }
